bf39fea6d4ceedd56599a583f67b1dcc96d5abe5
Jhonatan Contreras <jhonatancontreras2017@gmail.com>
2026-05-20 17:38:58 -0500

    ptm

--- script.js
+++ script.js
@@ -157,7 +157,7 @@ function agendarCita() {
   document.getElementById('dmn-result').classList.remove('show');
 
   updateStats();
-  
+
   fetch('api/webhook/nueva-cita', {
   method: 'POST',
   headers: { 'Content-Type': 'application/json' },
@@ -193,3 +193,5 @@ function showToast(msg, err=false) {
 
 // ─── INICIALIZACIÓN ───────────────────────────────────────────────
 updateStats();
+
+// mierda
